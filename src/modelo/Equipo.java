9b5b862f19c3f6b991bd71fcd1ca8745c00f4e61
lucasgallardo <lucagallardo@gmail.com>
2017-12-11 21:04:57 -0300

    first commit

---
+++ src/modelo/Equipo.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author lucas
+ */
+public class Equipo {
+    
+    int id;
+    String nombre;
+    int puntos;
+    int partidosGanados;
+    int partidosEmpatados;
+    int partidosPerdidos;
+    int golesFavor;
+    int golesContra;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getPartidosGanados() {
+        return partidosGanados;
+    }
+
+    public void setPartidosGanados(int partidosGanados) {
+        this.partidosGanados = partidosGanados;
+    }
+
+    public int getPartidosEmpatados() {
+        return partidosEmpatados;
+    }
+
+    public void setPartidosEmpatados(int partidosEmpatados) {
+        this.partidosEmpatados = partidosEmpatados;
+    }
+
+    public int getPartidosPerdidos() {
+        return partidosPerdidos;
+    }
+
+    public void setPartidosPerdidos(int partidosPerdidos) {
+        this.partidosPerdidos = partidosPerdidos;
+    }
+
+    public int getGolesFavor() {
+        return golesFavor;
+    }
+
+    public void setGolesFavor(int golesFavor) {
+        this.golesFavor = golesFavor;
+    }
+
+    public int getGolesContra() {
+        return golesContra;
+    }
+
+    public void setGolesContra(int golesContra) {
+        this.golesContra = golesContra;
+    }
+}
---
+++ src/modelo/Partido.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author lucas
+ */
+public class Partido {
+    
+    int fecha;
+    Equipo equipo1;
+    Equipo equipo2;
+    int goles1;
+    int goles2;
+
+    public int getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(int fecha) {
+        this.fecha = fecha;
+    }
+
+    public Equipo getEquipo1() {
+        return equipo1;
+    }
+
+    public void setEquipo1(Equipo equipo1) {
+        this.equipo1 = equipo1;
+    }
+
+    public Equipo getEquipo2() {
+        return equipo2;
+    }
+
+    public void setEquipo2(Equipo equipo2) {
+        this.equipo2 = equipo2;
+    }
+
+    public int getGoles1() {
+        return goles1;
+    }
+
+    public void setGoles1(int goles1) {
+        this.goles1 = goles1;
+    }
+
+    public int getGoles2() {
+        return goles2;
+    }
+
+    public void setGoles2(int goles2) {
+        this.goles2 = goles2;
+    }
+    
+}
